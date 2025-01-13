6e711a0ab37baf644b5be3ea888da7a1f5369ca6
Shinn <zhouxy.vortex@gmail.com>
2025-01-13 10:55:12 +0800

    update init lua.options fixed clipboard

--- init.lua
+++ init.lua
@@ -3,6 +3,14 @@ require 'shinn.configs.keymaps'
 -- Load vim option configuration file.
 require 'shinn.configs.options'
 
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
