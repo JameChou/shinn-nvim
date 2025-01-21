e57ef60f134f8910185c071e5d2731c6656ff69f
Shinn <zhouxy.vortex@gmail.com>
2025-01-21 14:36:15 +0800

    增加vista插件的支持

--- init.lua
+++ init.lua
@@ -67,6 +67,7 @@ require('lazy').setup({
   require 'shinn.plugins.vim-instant-markdown',
   require 'shinn.plugins.comment',
   require 'shinn.plugins.winbar',
+  require('shinn.plugins.vista').Config,
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
---
+++ lua/shinn/plugins/vista.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.Config = {
+  'liuchengxu/vista.vim',
+
+  config = function()
+    vim.g.vista_icon_indent = { '╰─▸ ', '├─▸ ' }
+    vim.g.vista_default_executive = 'ctags'
+
+    vim.keymap.set('n', 'T', '<cmd>Vista!!<CR>')
+  end,
+}
+
+return M
