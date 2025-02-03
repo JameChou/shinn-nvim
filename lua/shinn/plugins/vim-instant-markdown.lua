02d316d0d7020fb07793922bd88b19b13895093a
shinn <zhouxy.vortex@gmail.com>
2025-02-04 01:07:57 +0800

    change the instant-markdown-preview to markdown-preview.nvim

--- lua/shinn/plugins/vim-instant-markdown.lua
+++ lua/shinn/plugins/vim-instant-markdown.lua
@@ -1,9 +1,13 @@
 -- Markdown real-time preview plugin.
 return {
-  'instant-markdown/vim-instant-markdown',
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && yarn install',
+  init = function()
+    vim.g.mkdp_filetypes = { 'markdown' }
+  end,
   ft = { 'markdown' },
-  build = 'yarn install',
   config = function()
-    vim.g.instant_markdown_autostart = 0
+    vim.g.mkdp_auto_close = 0
   end,
 }
