1c7db333bdcfa34fa52121ba998bb1cb54f4742c
shinn <zhouxy.vortex@gmail.com>
2025-05-08 08:49:39 +0800

    增加neoscroll和smear-cursor的插件配置

---
+++ lua/shinn/plugins/neoscroll.lua
@@ -0,0 +1,6 @@
+return {
+  -- scroll page with animation
+  -- <C-u>, <C-d>, <C-f>, <C-b>
+  'karb94/neoscroll.nvim',
+  opts = {},
+}
---
+++ lua/shinn/plugins/smear-cursor.lua
@@ -0,0 +1,8 @@
+return {
+  -- smooth move the cursor with animations, plugin
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    stiffness = 0.5,
+    trailing_stiffness = 0.49,
+  },
+}
