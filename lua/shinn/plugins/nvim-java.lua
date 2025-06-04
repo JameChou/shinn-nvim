e6d53f1233c539f2289c6076ff71a9c70c5d2df1
shinn <zhouxy.vortex@gmail.com>
2025-06-04 15:22:03 +0800

    更新neovim java下的 jdtls的配置文件

--- lua/shinn/plugins/nvim-java.lua
+++ lua/shinn/plugins/nvim-java.lua
@@ -10,14 +10,27 @@
 return {
   'mfussenegger/nvim-jdtls',
   dependencies = 'saghen/blink.cmp',
-  config = function()
-    require('jdtls').start_or_attach {
-      cmd = {
-        vim.fn.expand '$HOME/.local/share/nvim/mason/bin/jdtls',
-        ('--jvm-arg=-javaagent:%s'):format(vim.fn.expand '$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar'),
-      },
-      capabilities = require('blink-cmp').get_lsp_capabilities(),
-      bundles = vim.split(vim.fn.glob('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', 1), '\n'),
-    }
+  opts = {
+    cmd = {
+      vim.fn.expand '$HOME/.local/share/nvim/mason/bin/jdtls',
+      ('--jvm-arg=-javaagent:%s'):format(vim.fn.expand '$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar'),
+    },
+    -- root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.idea', '.settings', 'mvnw' }, { upward = true })[1]),
+    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.idea', '.git', 'mvnw' }, { upward = true })[1]),
+    bundles = vim.split(vim.fn.glob('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', 1), '\n'),
+  },
+  config = function(_, opts)
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'java',
+      callback = function()
+        -- prints and pcall are there only to give quick feedback if it works.
+        -- print 'Starting JDTLS...'
+        opts.capabilities = require('blink-cmp').get_lsp_capabilities()
+        local success, result = pcall(require('jdtls').start_or_attach, opts)
+        if not success then
+          print('Error starting JDTLS: ' .. tostring(result))
+        end
+      end,
+    })
   end,
 }
