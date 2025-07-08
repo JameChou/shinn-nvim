d010295c0b968c0283338b23b0df3b9368833944
shinn <zhouxy.vortex@gmail.com>
2025-07-08 10:27:15 +0800

    修改一些主题

--- lua/shinn/plugins/themes/tokynight.lua
+++ lua/shinn/plugins/themes/tokynight.lua
@@ -6,14 +6,14 @@ return {
   },
   config = function()
     require('tokyonight').setup {
-      style = 'moon',         -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+      style = 'night',        -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
       light_style = 'day',    -- The theme is used when the background is set to light
       transparent = false,    -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
       styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = false },
+        comments = { italic = true },
         keywords = { italic = true, bold = false },
         functions = {},
         variables = {},
