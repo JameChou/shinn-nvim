a62a471ad7cc69c860265c8ac8519e7c2b87ea6f
Shinn <zhouxy.vortex@gmail.com>
2026-02-07 13:06:52 +0800

    去除nvim和kitty padding设置

--- init.lua
+++ init.lua
@@ -82,16 +82,16 @@ require('lazy').setup({
   },
 })
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.defer_fn(function()
-      vim.cmd("silent !kitty @ set-spacing padding=1")
-    end, 100)
-  end,
-})
-
-vim.api.nvim_create_autocmd("VimLeave", {
-  callback = function()
-    vim.cmd("silent !kitty @ set-spacing padding=10")
-  end,
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   callback = function()
+--     vim.defer_fn(function()
+--       vim.cmd("silent !kitty @ set-spacing padding=1")
+--     end, 100)
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("VimLeave", {
+--   callback = function()
+--     vim.cmd("silent !kitty @ set-spacing padding=10")
+--   end,
+-- })
